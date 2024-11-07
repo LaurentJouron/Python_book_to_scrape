ecf943b9266f05227aabb3abaafdcc712289283e
Laurent <jouronlaurent@hotmail.com>
2024-11-07 09:32:55 +0100

    Update __main__.py

--- scraping/__main__.py
+++ scraping/__main__.py
@@ -1,5 +1,12 @@
+# __main__.py
+
+# Import the Details class from the 'details' module
 from details import Details
 
+# Entry point of the script
 if __name__ == "__main__":
+    # Create an instance of the Details class
     details = Details()
+
+    # Execute the main functionality of the Details instance
     details.run()
