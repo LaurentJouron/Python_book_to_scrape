429b916ec62e596d0dc9ff3f47629e2cceeb4331
Laurent <jouronlaurent@hotmail.com>
2024-11-07 09:33:57 +0100

    Update constants.py

--- scraping/constants.py
+++ scraping/constants.py
@@ -1,4 +1,9 @@
+# constants.py
+
+# Base URL of the website to scrape
 WEBSITE = "https://books.toscrape.com/"
+
+# CSV headers corresponding to the extracted information for each book
 HEADERS = [
     "Name categories",
     "Links categories",
@@ -14,5 +19,9 @@ HEADERS = [
     "number_of_review",
     "synopsis",
 ]
+
+# Path for the folder where downloaded images will be saved
 MEDIA = "./media"
+
+# Path for the folder where generated CSV files will be saved
 CSV = "./csv"
